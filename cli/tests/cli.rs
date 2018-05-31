08c2cf64053d7d233f9aeb7b4f70b2adcf3eab0a
Jacob Peddicord <peddicor@amazon.com>
2018-05-31 15:30:20 -0700

    Add some basic CLI tests

---
+++ cli/tests/cli.rs
@@ -0,0 +1,66 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+#[macro_use]
+extern crate serde_json;
+
+use std::path::{Path, PathBuf};
+use std::process::Command;
+use std::process::Output;
+use std::str::from_utf8;
+
+use serde_json::Value;
+
+fn find_exe() -> PathBuf {
+    let me = std::env::current_exe().unwrap();
+    let dir = me.parent().unwrap();
+    let actual_dir = if dir.ends_with("deps") {
+        dir.parent().unwrap()
+    } else {
+        dir
+    };
+
+    actual_dir.join("askalono")
+}
+
+fn run(args: &[&str]) -> Output {
+    let exe = find_exe();
+    let out = Command::new(exe)
+        .args(args)
+        .output()
+        .expect("launch failed");
+    out
+}
+
+fn run_json(args: &[&str]) -> Value {
+    let cat = [&["--format=json"], args].concat();
+    let out = run(&cat);
+    serde_json::from_str(from_utf8(&out.stdout).expect("output was not utf8")).expect("output was not valid json")
+}
+
+#[test]
+fn cli_sanity() {
+    let out = run(&["id", "../LICENSE"]);
+    assert!(out.status.success());
+}
+
+#[test]
+fn output_json() {
+    let json = run_json(&["id", "../LICENSE"]);
+    assert_eq!("../LICENSE", json["path"]);
+    assert!(json["result"]["score"].as_f64().expect("score must be a number") > 0.90f64);
+    assert_eq!("Apache-2.0", json["result"]["license"]["name"]);
+    assert_eq!("original", json["result"]["license"]["kind"]);
+    assert_eq!(0, json["result"]["license"]["aliases"].as_array().expect("aliases must be an array").len());
+    assert_eq!(0, json["result"]["containing"].as_array().expect("containing must be an array").len());
+}
