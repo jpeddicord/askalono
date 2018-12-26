6e942de1bde9405e3a150200751e69cbfe144a30
Jacob Peddicord <peddicor@amazon.com>
2018-12-26 15:31:49 -0800

    Fix CLI help text
    Fixes #34

--- cli/src/commands.rs
+++ cli/src/commands.rs
@@ -14,58 +14,60 @@ arg_enum! {
 #[derive(StructOpt)]
 #[structopt(name = "askalono")]
 pub struct Opt {
+
+    /// Path to a cache file containing compiled license information
     #[structopt(long = "cache", short = "c", parse(from_os_str))]
     pub cache: Option<PathBuf>,
-    #[structopt(long = "format", help = "output type: text (default), json")]
+
+    /// Output type: text (default), json
+    #[structopt(long = "format")]
     #[structopt(raw(possible_values = "&OutputFormat::variants()"))]
     pub format: Option<OutputFormat>,
+
     #[structopt(subcommand)]
     pub subcommand: Subcommand,
 }
 
 #[derive(StructOpt)]
 pub enum Subcommand {
+    
+    /// Identify a single file
     #[structopt(name = "identify", alias = "id")]
     Identify {
-        #[structopt(
-            name = "FILE",
-            help = "file to identify",
-            required_unless = "batch",
-            parse(from_os_str)
-        )]
+        /// File to identify
+        #[structopt(name = "FILE", required_unless = "batch", parse(from_os_str))]
         filename: Option<PathBuf>,
-        #[structopt(
-            long = "optimize",
-            short = "o",
-            help = "try to find the location of a license within the file"
-        )]
+
+        /// Try to find the location of a license within the given file
+        #[structopt(long = "optimize", short = "o")]
         optimize: bool,
+
         #[structopt(raw(hidden = "true"))]
         #[structopt(long = "diff")]
         diff: bool,
-        #[structopt(
-            long = "batch",
-            short = "b",
-            help = "read in filenames on stdin"
-        )]
+
+        /// Read in filenames on stdin for batch identification
+        #[structopt(long = "batch", short = "b")]
         batch: bool,
     },
+
+    /// Crawl a directory identifying license files
     #[structopt(name = "crawl")]
     Crawl {
-        #[structopt(
-            name = "DIR",
-            help = "directory to crawl",
-            parse(from_os_str)
-        )]
+        /// Directory to crawl
+        #[structopt(name = "DIR", parse(from_os_str))]
         directory: PathBuf,
-        #[structopt(long = "follow", help = "follow symlinks")]
+
+        /// Follow symlinks
+        #[structopt(long = "follow")]
         follow_links: bool,
-        #[structopt(
-            long = "glob",
-            help = "glob of files to check (defaults to license-like files)"
-        )]
+
+        /// Glob of files to check (defaults to license-like files)
+        #[structopt(long = "glob")]
         glob: Option<String>,
     },
+
+    /// Cache management actions
     #[structopt(name = "cache")]
     Cache {
         #[structopt(subcommand)]
@@ -75,18 +77,16 @@ pub enum Subcommand {
 
 #[derive(StructOpt)]
 pub enum CacheSubcommand {
+
+    /// Load an SPDX license directory (see https://github.com/spdx/license-list-data/tree/master/json/details for format)
     #[structopt(name = "load-spdx")]
     LoadSpdx {
-        #[structopt(
-            name = "DIR",
-            help = "JSON details directory",
-            parse(from_os_str)
-        )]
+        /// JSON "details" directory
+        #[structopt(name = "DIR", parse(from_os_str))]
         dir: PathBuf,
-        #[structopt(
-            long = "store",
-            help = "store texts in cache along with match data"
-        )]
+
+        /// Store texts in cache along with match data
+        #[structopt(long = "store")]
         store_texts: bool,
     },
 }
