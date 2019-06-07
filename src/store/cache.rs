0cb0639beb072890be19b8c734d0413db6ac1169
Jake Shadle <jake.shadle@embark-studios.com>
2019-06-07 17:34:55 +0200

    Replace deflate with zstd

--- src/store/cache.rs
+++ src/store/cache.rs
@@ -3,15 +3,14 @@
 
 use std::{io::copy, io::prelude::*};
 
-use failure::{bail, format_err, Error};
-use flate2::{read::GzDecoder, Compression, GzBuilder};
+use failure::Error;
 use log::info;
 use rmp_serde::Serializer;
 use serde::Serialize;
 
 use crate::store::base::Store;
 
-const CACHE_VERSION: &[u8] = b"askalono-03";
+const CACHE_VERSION: &[u8] = b"askalono-04";
 
 impl Store {
     /// Create a store from a cache file.
@@ -21,25 +20,19 @@ impl Store {
     /// the full SPDX set from disk in 200-300 ms. The cache will be
     /// sanity-checked to ensure it was generated with a similar version of
     /// askalono.
-    pub fn from_cache<R>(readable: R) -> Result<Store, Error>
+    pub fn from_cache<R>(mut readable: R) -> Result<Store, Error>
     where
         R: Read + Sized,
     {
-        use rmp_serde::decode::from_read;
+        let mut header = [0u8; 11];
+        readable.read_exact(&mut header)?;
 
-        let dec = GzDecoder::new(readable);
-        {
-            let extra = dec
-                .header()
-                .ok_or_else(|| format_err!("cache gzip header invalid"))?
-                .extra()
-                .ok_or_else(|| format_err!("cache gzip extra header missing"))?;
-            if extra != CACHE_VERSION {
-                bail!("cache version mismatch");
-            }
+        if header != CACHE_VERSION {
+            failure::bail!("cache version mismatch");
         }
 
-        let store = from_read(dec)?;
+        let dec = zstd::Decoder::new(readable)?;
+        let store = rmp_serde::decode::from_read(dec)?;
         Ok(store)
     }
 
@@ -59,10 +52,11 @@ impl Store {
 
         info!("Pre-compressed output is {} bytes", buf.len());
 
-        let mut gz = GzBuilder::new()
-            .extra(CACHE_VERSION)
-            .write(&mut writable, Compression::best());
-        copy(&mut buf.as_slice(), &mut gz)?;
+        writable.write_all(CACHE_VERSION)?;
+        let mut zenc = zstd::Encoder::new(writable, 21)?;
+
+        copy(&mut buf.as_slice(), &mut zenc)?;
+        zenc.finish()?;
 
         Ok(())
     }
