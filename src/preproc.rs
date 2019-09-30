39f6e228775e17266bcba46f48b26b01bbfb7698
Jacob Peddicord <peddicor@amazon.com>
2019-09-30 13:26:03 -0700

    Use Cow<str> instead of Strings where relevant 🐄
    This _should_ save on some needless allocations. More work to be done.

--- src/preproc.rs
+++ src/preproc.rs
@@ -6,10 +6,23 @@ use std::collections::HashMap;
 
 use lazy_static::lazy_static;
 use log::debug;
-use regex::Regex;
+use regex::{Regex, Replacer};
 use unicode_normalization::UnicodeNormalization;
 
-type PreprocFn = dyn Fn(&str) -> String;
+type PreprocFn = dyn Fn(Cow<str>) -> Cow<str>;
+
+trait CowRegex {
+    fn replace_all_cow<'a, R: Replacer>(&self, text: Cow<'a, str>, replace: R) -> Cow<'a, str>;
+}
+
+impl CowRegex for Regex {
+    fn replace_all_cow<'a, R: Replacer>(&self, text: Cow<'a, str>, replace: R) -> Cow<'a, str> {
+        match text {
+            Cow::Borrowed(find) => self.replace_all(find, replace),
+            Cow::Owned(find) => Cow::Owned(self.replace_all(&find, replace).into_owned()),
+        }
+    }
+}
 
 /// A list of preprocessors that normalize text without removing anything
 /// substantial. These operate on one line at a time.
@@ -19,7 +32,7 @@ pub const PREPROC_NORMALIZE: [&PreprocFn; 6] = [
     &blackbox_urls,
     &normalize_horizontal_whitespace,
     &normalize_punctuation,
-    &trim_line,
+    &trim,
 ];
 
 /// A list of preprocessors that more aggressively normalize/mangle text
@@ -33,60 +46,60 @@ pub const PREPROC_AGGRESSIVE: [&PreprocFn; 8] = [
     &remove_title_line,
     &remove_copyright_statements,
     &collapse_whitespace,
-    &final_trim,
+    &trim,
 ];
 
 pub fn apply_normalizers(text: &str) -> Vec<String> {
     let mut lines = Vec::new();
     for line in text.split('\n') {
-        let mut out = line.to_owned();
+        let mut out: Cow<str> = line.into();
         for preproc in &PREPROC_NORMALIZE {
-            out = preproc(&out);
+            out = preproc(out);
         }
-        lines.push(out);
+        lines.push(out.into());
     }
     debug!("Normalized to:\n{:?}\n---", lines);
     lines
 }
 
 pub fn apply_aggressive(text: &str) -> String {
-    let mut out = text.to_owned();
+    let mut out = text.into();
     for preproc in &PREPROC_AGGRESSIVE {
-        out = preproc(&out);
+        out = preproc(out);
     }
-    debug!("Aggressively normalized to:\n{}\n---", out);
-    out
+    debug!("Aggressively normalized to:\n{}\n---", &out);
+    out.into()
 }
 
 // Line-by-line normalizers
 
-fn normalize_unicode(input: &str) -> String {
-    input.nfc().collect::<String>()
+fn normalize_unicode(input: Cow<str>) -> Cow<str> {
+    input.nfc().collect::<String>().into()
 }
 
-fn remove_junk(input: &str) -> String {
+fn remove_junk(input: Cow<str>) -> Cow<str> {
     lazy_static! {
         static ref RX: Regex = Regex::new(r"[^\w\s\pP]+").unwrap();
     }
-    RX.replace_all(input, "").into()
+    RX.replace_all_cow(input, "")
 }
 
-fn blackbox_urls(input: &str) -> String {
+fn blackbox_urls(input: Cow<str>) -> Cow<str> {
     lazy_static! {
         static ref RX: Regex = Regex::new(r"https?://\S+").unwrap();
     }
-    RX.replace_all(input, "http://blackboxed/url").into()
+    RX.replace_all_cow(input, "http://blackboxed/url")
 }
 
-fn normalize_horizontal_whitespace(input: &str) -> String {
+fn normalize_horizontal_whitespace(input: Cow<str>) -> Cow<str> {
     lazy_static! {
         // including slashes here as well
         static ref RX: Regex = Regex::new(r"(?x)[ \t\p{Zs} \\ / \| \x2044 ]+").unwrap();
     }
-    RX.replace_all(input, " ").into()
+    RX.replace_all_cow(input, " ")
 }
 
-fn normalize_punctuation(input: &str) -> String {
+fn normalize_punctuation(input: Cow<str>) -> Cow<str> {
     lazy_static! {
         static ref RX_QUOTES: Regex = Regex::new(r#"["'\p{Pi}\p{Pf}]+"#).unwrap();
         static ref RX_DASH: Regex = Regex::new(r"\p{Pd}+").unwrap();
@@ -95,18 +108,21 @@ fn normalize_punctuation(input: &str) -> String {
         static ref RX_UNDER: Regex = Regex::new(r"\p{Pc}+").unwrap();
         static ref RX_COPY: Regex = Regex::new(r"[©Ⓒⓒ]").unwrap();
     }
-    let out = Cow::Borrowed(input);
-    let out = RX_QUOTES.replace_all(&out, "'");
-    let out = RX_DASH.replace_all(&out, "-");
-    let out = RX_OPEN.replace_all(&out, "(");
-    let out = RX_CLOSE.replace_all(&out, ")");
-    let out = RX_UNDER.replace_all(&out, "_");
-    let out = RX_COPY.replace_all(&out, "(c)");
-    out.into()
+    let mut out = input;
+    out = RX_QUOTES.replace_all_cow(out, "'");
+    out = RX_DASH.replace_all_cow(out, "-");
+    out = RX_OPEN.replace_all_cow(out, "(");
+    out = RX_CLOSE.replace_all_cow(out, ")");
+    out = RX_UNDER.replace_all_cow(out, "_");
+    out = RX_COPY.replace_all_cow(out, "(c)");
+    out
 }
 
-fn trim_line(input: &str) -> String {
-    input.trim().into()
+fn trim(input: Cow<str>) -> Cow<str> {
+    match input {
+        Cow::Borrowed(text) => text.trim().into(),
+        Cow::Owned(text) => Cow::Owned(text.trim().to_owned()),
+    }
 }
 
 // Aggressive preprocessors
@@ -126,8 +142,8 @@ fn lcs_substr(f_line: &str, s_line: &str) -> String {
         .into() //TODO: big optimization needed, this is a wasteful conversion
 }
 
-fn remove_common_tokens(text: &str) -> String {
-    let lines: Vec<&str> = text.split('\n').collect();
+fn remove_common_tokens(input: Cow<str>) -> Cow<str> {
+    let lines: Vec<&str> = input.split('\n').collect();
     let mut l_iter = lines.iter().peekable();
 
     // TODO: consider whether this can all be done in one pass https://github.com/amzn/askalono/issues/36
@@ -151,7 +167,7 @@ fn remove_common_tokens(text: &str) -> String {
     // look at the most common observed prefix
     let max_prefix = prefix_counts.iter().max_by_key(|&(_k, v)| v);
     if max_prefix.is_none() {
-        return text.to_string();
+        return input.into();
     }
     let (most_common, _) = max_prefix.unwrap();
 
@@ -166,7 +182,7 @@ fn remove_common_tokens(text: &str) -> String {
     // the common string must be at least 80% of the text
     let prefix_threshold: u32 = (0.8f32 * lines.len() as f32) as u32;
     if final_common_count < prefix_threshold {
-        return text.to_string();
+        return input.into();
     }
 
     // pass 2: remove that substring
@@ -183,39 +199,40 @@ fn remove_common_tokens(text: &str) -> String {
         .map(|line| line.trim())
         .collect::<Vec<_>>()
         .join("\n")
+        .into()
 }
 
-fn normalize_vertical_whitespace(input: &str) -> String {
+fn normalize_vertical_whitespace(input: Cow<str>) -> Cow<str> {
     lazy_static! {
         static ref RX_MISC: Regex = Regex::new(r"[\r\n\v\f]").unwrap();
         static ref RX_NUM: Regex = Regex::new(r"\n{3,}").unwrap();
     }
-    let out = Cow::Borrowed(input);
-    let out = RX_MISC.replace_all(&out, "\n");
-    let out = RX_NUM.replace_all(&out, "\n\n");
-    out.into()
+    let mut out = input;
+    out = RX_MISC.replace_all_cow(out, "\n");
+    out = RX_NUM.replace_all_cow(out, "\n\n");
+    out
 }
 
-fn remove_punctuation(input: &str) -> String {
+fn remove_punctuation(input: Cow<str>) -> Cow<str> {
     lazy_static! {
         static ref RX: Regex = Regex::new(r"[^\w\s]+").unwrap();
     }
-    RX.replace_all(input, "").into()
+    RX.replace_all_cow(input, "")
 }
 
-fn lowercaseify(input: &str) -> String {
-    input.to_lowercase()
+fn lowercaseify(input: Cow<str>) -> Cow<str> {
+    input.to_lowercase().into()
 }
 
-fn remove_title_line(input: &str) -> String {
+fn remove_title_line(input: Cow<str>) -> Cow<str> {
     lazy_static! {
         static ref RX: Regex = Regex::new(r"^.*license( version \S+)?( copyright.*)?\n\n").unwrap();
     }
 
-    RX.replace_all(input, "").into()
+    RX.replace_all_cow(input, "")
 }
 
-fn remove_copyright_statements(input: &str) -> String {
+fn remove_copyright_statements(input: Cow<str>) -> Cow<str> {
     lazy_static! {
         static ref RX: Regex = Regex::new(
             r"(?mx)
@@ -241,18 +258,14 @@ fn remove_copyright_statements(input: &str) -> String {
         .unwrap();
     }
 
-    RX.replace_all(input, "\n\n").into()
+    RX.replace_all_cow(input, "\n\n").into()
 }
 
-fn collapse_whitespace(input: &str) -> String {
+fn collapse_whitespace(input: Cow<str>) -> Cow<str> {
     lazy_static! {
         static ref RX: Regex = Regex::new(r"\s+").unwrap();
     }
-    RX.replace_all(input, " ").into()
-}
-
-fn final_trim(input: &str) -> String {
-    input.trim().into()
+    RX.replace_all_cow(input, " ")
 }
 
 #[cfg(test)]
@@ -271,7 +284,7 @@ mod tests {
                     \n\
                     abcd";
 
-        let new_text = remove_common_tokens(text);
+        let new_text = remove_common_tokens(text.into());
         println!("{}", new_text);
 
         assert_eq!(
@@ -286,7 +299,7 @@ mod tests {
         let text = "this string should still have\n\
                     this word -> this <- in it even though\n\
                     this is still the most common word";
-        let new_text = remove_common_tokens(text);
+        let new_text = remove_common_tokens(text.into());
         println!("-- {}", new_text);
         // the "this" at the start of the line can be discarded...
         assert!(!new_text.contains("\nthis"));
@@ -296,7 +309,7 @@ mod tests {
         let text = "aaaa bbbb cccc dddd\n\
                     eeee ffff aaaa gggg\n\
                     hhhh iiii jjjj";
-        let new_text = remove_common_tokens(text);
+        let new_text = remove_common_tokens(text.into());
         println!("-- {}", new_text);
         assert!(new_text.contains("aaaa")); // similar to above test
     }
@@ -319,7 +332,7 @@ mod tests {
                     \n\
                     preserve\n\
                     keep";
-        let new_text = remove_common_tokens(text);
+        let new_text = remove_common_tokens(text.into());
         println!("{}", new_text);
 
         assert!(new_text.contains("preserve"));
